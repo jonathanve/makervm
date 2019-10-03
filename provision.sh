9c14242d21db3c39fc652559a4393a326e44f65d
Jonathan Valencia <jonathanv.espitia@gmail.com>
2019-10-03 09:33:54 -0500

    update go and grpc versions

--- provision.sh
+++ provision.sh
@@ -68,7 +68,7 @@ usermod -a -G docker vagrant
 service docker start
 
 # vars
-export GO_VERSION=1.13
+export GO_VERSION=1.13.1
 export GO_ARCH=linux-amd64
 export GO_URL=https://golang.org/dl/go${GO_VERSION}.${GO_ARCH}.tar.gz
 
@@ -79,7 +79,7 @@ export GOARCH=amd64
 
 export PROTOC_PATH=/vagrant_data/software/protoc-3.8.0-linux-x86_64
 export GRPC_PATH=/home/vagrant/grpc
-export GRPC_VERSION=v1.22.1
+export GRPC_VERSION=v1.24.0
 
 # protoc
 sudo -u vagrant mkdir -p $PROTOC_PATH
